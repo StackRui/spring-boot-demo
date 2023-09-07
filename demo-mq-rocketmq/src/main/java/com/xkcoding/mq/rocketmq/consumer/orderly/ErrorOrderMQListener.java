01c80364bdc9382ea2b403c51322909592b6937a
rui.hou <rui.hou@linkflowtech.com>
2023-09-07 17:47:45 +0800

    message retry

---
+++ demo-mq-rocketmq/src/main/java/com/xkcoding/mq/rocketmq/consumer/orderly/ErrorOrderMQListener.java
@@ -0,0 +1,21 @@
+package com.xkcoding.mq.rocketmq.consumer.orderly;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.annotation.SelectorType;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RocketMQMessageListener(topic = "order", consumerGroup = "error-order-group", consumeThreadMax = 1, selectorType = SelectorType.SQL92, selectorExpression = "items<=30")
+public class ErrorOrderMQListener implements RocketMQListener<MessageExt> {
+
+  @Override
+  public void onMessage(MessageExt message) {
+    // 处理收到的消息
+    System.out.println("Received message: " + new String(message.getBody()) + ", key = " + message.getKeys() + ", tag = " + message.getTags() + ", properties = " + JSON.toJSONString(message.getProperties()));
+    throw new RuntimeException("处理错误");
+
+  }
+}
--- demo-mq-rocketmq/src/main/java/com/xkcoding/mq/rocketmq/controller/ProducerController.java
+++ demo-mq-rocketmq/src/main/java/com/xkcoding/mq/rocketmq/controller/ProducerController.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import org.apache.commons.lang3.RandomUtils;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.MessageConst;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/rocketmq")
+@RequestMapping("/rocketmq/order")
 public class ProducerController {
   private final RocketMQTemplate rocketMQTemplate;
 
@@ -23,18 +24,15 @@ public class ProducerController {
   @PostMapping("/syncSendOrderly")
   @ResponseBody
   public SendResult syncSendOrderly(@RequestBody String str, @RequestParam String orderId) {
-    Map<String, Object> headers = new HashMap<>();
-    headers.put("KEYS", orderId);
-    headers.put("items", RandomUtils.nextInt(25, 50));
+    Map<String, Object> headers = getHeaders(orderId);
     return rocketMQTemplate.syncSendOrderly("order:" + orderId, new GenericMessage<>(str, headers), orderId);
   }
 
   @PostMapping("/asyncSendOrderly")
   @ResponseBody
   public void asyncSendOrderly(@RequestBody String str, @RequestParam String orderId) {
-    Map<String, Object> headers = new HashMap<>();
-    headers.put("KEYS", orderId);
-    headers.put("items", RandomUtils.nextInt(25, 50));
+    Map<String, Object> headers = getHeaders(orderId);
+
 
     rocketMQTemplate.asyncSendOrderly("order:" + orderId, new GenericMessage<>(str, headers), orderId, new SendCallback() {
       @Override
@@ -52,11 +50,18 @@ public class ProducerController {
   @PostMapping("/sendOneWay")
   @ResponseBody
   public void sendOneWay(@RequestBody String str, @RequestParam String orderId) {
-    Map<String, Object> headers = new HashMap<>();
-    headers.put("KEYS", orderId);
-    headers.put("items", RandomUtils.nextInt(25, 50));
+    Map<String, Object> headers = getHeaders(orderId);
 
     // 单向发送无感知
     rocketMQTemplate.sendOneWay("order:" + orderId, new GenericMessage<>(str, headers));
+  }
+
+  private static Map<String, Object> getHeaders(String orderId) {
+    Map<String, Object> headers = new HashMap<>();
+    headers.put(MessageConst.PROPERTY_KEYS, orderId);
+    headers.put("items", RandomUtils.nextInt(25, 50));
+    headers.put(MessageConst.PROPERTY_MAX_RECONSUME_TIMES, 1);
+    headers.put("maxReconsumeTimes", 1);
+    return headers;
   }
 }
