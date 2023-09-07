2081a550c728ca4187cf05f89b8264e309d3936c
rui.hou <rui.hou@linkflowtech.com>
2023-09-07 15:13:29 +0800

    add async orderly producer endpoint

--- demo-mq-rocketmq/src/main/java/com/xkcoding/mq/rocketmq/controller/ProducerController.java
+++ demo-mq-rocketmq/src/main/java/com/xkcoding/mq/rocketmq/controller/ProducerController.java
@@ -1,9 +1,10 @@
 package com.xkcoding.mq.rocketmq.controller;
 
+import com.alibaba.fastjson.JSON;
 import org.apache.commons.lang3.RandomUtils;
+import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,17 +14,38 @@ import java.util.Map;
 @RestController
 @RequestMapping("/send")
 public class ProducerController {
-  @Autowired
-  private RocketMQTemplate rocketMQTemplate;
+  private final RocketMQTemplate rocketMQTemplate;
+
+  public ProducerController(RocketMQTemplate rocketMQTemplate) {
+    this.rocketMQTemplate = rocketMQTemplate;
+  }
 
   @PostMapping("/syncSendOrderly")
   @ResponseBody
   public SendResult syncSendOrderly(@RequestBody String str, @RequestParam String orderId) {
-    // 构建消息对象
     Map<String, Object> headers = new HashMap<>();
     headers.put("KEYS", orderId);
-    headers.put("items", RandomUtils.nextInt(25,50));
-    // 设置消息的 key
-    return rocketMQTemplate.syncSendOrderly("order:"+orderId, new GenericMessage<>(str, headers), orderId);
+    headers.put("items", RandomUtils.nextInt(25, 50));
+    return rocketMQTemplate.syncSendOrderly("order:" + orderId, new GenericMessage<>(str, headers), orderId);
+  }
+
+  @PostMapping("/asyncSendOrderly")
+  @ResponseBody
+  public void asyncSendOrderly(@RequestBody String str, @RequestParam String orderId) {
+    Map<String, Object> headers = new HashMap<>();
+    headers.put("KEYS", orderId);
+    headers.put("items", RandomUtils.nextInt(25, 50));
+
+    rocketMQTemplate.asyncSendOrderly("order:" + orderId, new GenericMessage<>(str, headers), orderId, new SendCallback() {
+      @Override
+      public void onSuccess(SendResult sendResult) {
+        System.out.printf("发送消息成功!\n" + JSON.toJSONString(sendResult));
+      }
+
+      @Override
+      public void onException(Throwable throwable) {
+        System.out.printf("发送消息失败!\n" + throwable.getMessage());
+      }
+    });
   }
 }
