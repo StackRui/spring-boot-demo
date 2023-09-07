bdbf3a661a2d1ae6d849d2e3c10b0e2ecd489d7a
rui.hou <rui.hou@linkflowtech.com>
2023-09-07 15:16:32 +0800

    change async orderly producer endpoint

--- demo-mq-rocketmq/src/main/java/com/xkcoding/mq/rocketmq/controller/ProducerController.java
+++ demo-mq-rocketmq/src/main/java/com/xkcoding/mq/rocketmq/controller/ProducerController.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/send")
+@RequestMapping("/rocketmq")
 public class ProducerController {
   private final RocketMQTemplate rocketMQTemplate;
 
